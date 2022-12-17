a2ffd4c4c140da02a9151879240c62b962bb6f1d
LiangLliu <max1141847753@gmail.com>
2022-12-17 21:11:32 +0800

    feat: table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -31,3 +31,21 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 }
+
+// TableDrivenTests See https://github.com/golang/go/wiki/TableDrivenTests
+func TestArea2(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+}
