608eb060998f7ce6a9592365208416693175d9db
LiangLliu <max1141847753@gmail.com>
2022-12-17 21:00:13 +0800

    feat: go interface using

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,14 +1,29 @@
 package structs
 
+import "math"
+
+// Shape interface resolution 是隐式的。如果传入的类型匹配接口需要的，则编译正确
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
